cbe2fa985e340d48f9c7908b87e49b567d08725c
Louis Thompson <thompsonl0109@gmail.com>
2023-04-26 15:27:43 +0100

    created inventory and amount amount of money

---
+++ VendingMachine/VendingMachine.cs
@@ -0,0 +1,63 @@
+
+
+using System;
+using System.Collections.Generic;
+
+namespace VendingMachine;
+
+public class VendingMachine
+{
+
+    //Contains Item name and value (Price)
+    private Dictionary<string, int> inventory;
+    private int balance;
+
+    // Properties
+    private int currentAmount;
+    private string currentSelection;
+
+    // Constructor
+    public VendingMachine()
+    {
+         // Initialize properties
+        currentAmount = 0;
+        inventory = new Dictionary<string, int>();
+    }
+
+    
+    public void SelectProduct(string product)
+    {
+        // Check if product is in stock
+        // Check if current amount is sufficient
+        // Dispense product and update display
+        // If not, display price or not enough money
+    }
+
+    public void MakeChange()
+    {
+        // Calculate change
+        // Dispense change and update display
+    }
+
+    public void ReturnCoins()
+    {
+        // Return current amount to user
+        // Update display
+    }
+
+    public bool IsProductSoldOut(string product)
+    {
+        // Check if product is sold out
+        // Update display
+        return true;
+    }
+
+    public bool IsExactChangeRequired()
+    {
+        // Check if exact change is required
+        // Update display
+        return true;
+    }
+
+
+}
